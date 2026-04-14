487fd00f97c1c29f56ee4e38e9636d0c452e9c93
aistriúcháin <109623581+aistriuchain@users.noreply.github.com>
2026-04-14 21:32:12 +0100

    Update articles.go

--- news/tuairisc/articles.go
+++ news/tuairisc/articles.go
@@ -1,1 +1,23 @@
+package tuairisc
 
+import (
+	"NewsChannel/news"
+)
+
+func (a *tuairisc) GetArticles() ([]news.Article, error) {
+	var articles []news.Article
+
+	temp, err := a.GetNationalArticles()
+	if err != nil {
+		return nil, err
+	}
+	articles = append(articles, temp...)
+
+	return articles, nil
+}
+
+// Need to find out how articles are actually integrated within the channel
+
+func (a *tuairisc) GetNationalArticles() ([]news.Article, error) {
+	return a.getArticles("https://www.tuairisc.ie/rss", news.NationalNews)
+}
