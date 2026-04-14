a216fe8794f538080a22640fad04b739143c2db1
aistriúcháin <109623581+aistriuchain@users.noreply.github.com>
2026-04-14 21:42:38 +0100

    Update new.go
    if this works by some miracle, i might go wild

--- news/tuairisc/new.go
+++ news/tuairisc/new.go
@@ -1,1 +1,27 @@
+package tuairisc
 
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"time"
+	"unicode/utf16"
+)
+
+//go:embed logo.jpg
+var Logo []byte
+
+func NewTuairisc(oldArticleTitles []string) *tuairisc {
+	return &tuairisc{
+		oldArticleTitles: oldArticleTitles,
+	}
+}
+
+func (a *tuairisc) GetLogo() []byte {
+	return Logo
+}
+
+func (a *tuairisc) GetCopyright() []uint16 {
+	copyrightString := fmt.Sprintf("© %s Copyright Tuairisc.ie - Gach ceart ar cosaint.", strconv.Itoa(time.Now().Year()))
+	return utf16.Encode([]rune(copyrightString))
+}
